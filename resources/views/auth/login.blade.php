a485a23e52c474df5f10158be84ea7ae3ae33de6
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-19 16:46:38 +0700

    login view page (step 2, remove register form)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -233,55 +233,11 @@
                                 <a href="#" title="" class="forgot-pwd">Forgot Password?</a>
                                 <div class="submit-btns">
                                     <button class="mtr-btn signin" type="button"><span>Login</span></button>
-                                    <button class="mtr-btn signup" type="button"><span>Register</span></button>
+                                    <a class="signup" style="margin-left: 30px;" href="{{url('/register')}}"><span>Register</span></a>
                                 </div>
                             </form>
                         </div>
-                        <div class="log-reg-area reg">
-                            <h2 class="log-title">Register</h2>
-                            <p>
-                                Don’t use Winku Yet? <a href="#" title="">Take the tour</a> or <a href="#" title="">Join now</a>
-                            </p>
-                            <form method="post">
-                                <div class="form-group">
-                                    <input type="text" required="required" />
-                                    <label class="control-label" for="input">First & Last Name</label><i class="mtrl-select"></i>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" required="required" />
-                                    <label class="control-label" for="input">User Name</label><i class="mtrl-select"></i>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" required="required" />
-                                    <label class="control-label" for="input">Password</label><i class="mtrl-select"></i>
-                                </div>
-                                <div class="form-radio">
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" name="radio" checked="checked" /><i class="check-box"></i>Male
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" name="radio" /><i class="check-box"></i>Female
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" required="required" />
-                                    <label class="control-label" for="input"><a href="http://www.wpkixx.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="70351d11191c30">[email&#160;protected]</a></label><i class="mtrl-select"></i>
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" checked="checked" /><i class="check-box"></i>Accept Terms & Conditions ?
-                                    </label>
-                                </div>
-                                <a href="#" title="" class="already-have">Already have an account</a>
-                                <div class="submit-btns">
-                                    <button class="mtr-btn signup" type="button"><span>Register</span></button>
-                                </div>
-                            </form>
-                        </div>
+
                     </div>
                 </div>
             </div>
