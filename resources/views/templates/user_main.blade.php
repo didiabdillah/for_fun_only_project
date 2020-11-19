5134ded2a0668b3e571b775bc00e00f385b5ee40
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-19 10:32:46 +0700

    start make root view blueprint from template

---
+++ resources/views/templates/user_main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.wpkixx.com/html/winku/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 09 Nov 2020 07:59:45 GMT -->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>@yield('title')</title>
+    <link rel="icon" href="images/fav.png" type="image/png" sizes="16x16">
+
+    <link rel="stylesheet" href="{{URL::asset('assets/css/main.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/color.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/responsive.css')}}">
+
+</head>
+
+<body>
+
+    @yield('page')
+
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="{{URL::asset('assets/js/main.min.js')}}"></script>
+    <script src="{{URL::asset('assets/js/script.js')}}"></script>
+    <script src="{{URL::asset('assets/js/map-init.js')}}"></script>
+
+</body>
+
+<!-- Mirrored from www.wpkixx.com/html/winku/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 09 Nov 2020 08:03:25 GMT -->
+
+</html>
